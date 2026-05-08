6a4300fcebb9fefa006ddac0bc3ed008d88dfdec
Erick <erick.semperteguif@gmail.com>
2026-05-08 12:22:30 -0400

    Fix: Error en authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const {verificarToken} = require('../middleware/authMiddleware');
+const {getAsesores} = require('../controllers/authController');
 const { registrarUsuario, loginUsuario } = require('../controllers/authController');
 
 // Ruta para registrar: POST http://localhost:3000/api/auth/register
 // ... (lo que ya tenías)
 router.post('/register', registrarUsuario);
 router.post('/login', loginUsuario); // NUEVA RUTA
-
+router.get('/asesores', verificarToken, getAsesores);
 module.exports = router;
